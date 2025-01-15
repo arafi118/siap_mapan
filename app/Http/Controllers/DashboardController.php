5bca3711f41884d47db5a18bdea4db6a9400db6a
arafi118 <arp46301@gmail.com>
2025-01-15 13:03:21 +0700

    Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Installations;
+use App\Models\Settings;
 use App\Models\Usage;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
@@ -41,21 +42,11 @@ class DashboardController extends Controller
             'customer',
             'package'
         ])->get();
-        $Blokir = Installations::where('status', 'B')->with([
-            'customer',
-            'package'
-        ])->get();
-        $Cabut = Installations::where('status', 'C')->with([
-            'customer',
-            'package'
-        ])->get();
 
         return response()->json([
             'Permohonan' => $Permohonan,
             'Pasang' => $Pasang,
-            'Aktif' => $Aktif,
-            'Blokir' => $Blokir,
-            'Cabut' => $Cabut
+            'Aktif' => $Aktif
         ]);
     }
 
@@ -64,7 +55,9 @@ class DashboardController extends Controller
         $Usages = Installations::where('status', 'A')->with([
             'customer',
             'package',
-            'usage'
+            'oneUsage' => function ($query) {
+                $query->where('tgl_akhir', '<=', date('Y-m-d'));
+            }
         ])->get();
 
         return response()->json([
@@ -83,9 +76,24 @@ class DashboardController extends Controller
             'installation.customer',
             'installation.package'
         ])->get();
+        $setting = Settings::where('business_id', Session::get('business_id'))->first();
+
+        $result = [];
+        $block = json_decode($setting->block, true);
+        foreach ($block as $index => $item) {
+            preg_match_all('/\d+/', $item['jarak'], $matches);
+            $start = (int)$matches[0][0];
+            $end = (int)$matches[0][1];
+
+            for ($i = $start; $i <= $end; $i++) {
+                $result[$i] = $index;
+            }
+        }
 
         return response()->json([
-            'Tagihan' => $Tagihan
+            'Tagihan' => $Tagihan,
+            'setting' => $setting,
+            'block' => $result
         ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
                     <div class="display-4 fs-5 mb-2 font-weight-normal text-success" id="InstallationCount">
                         {{ $Installation }}
                     </div>
-                    <a class="font-weight-bold text-nowrap fs-10" href="../app/e-commerce/customers.html">
-                        See all
+                    <a class="font-weight-bold text-nowrap fs-10" href="#" id="BtnModalInstalasi">
+                        Lihat Detail
                         <span class="fas fa-angle-right mr-1"></span>
                     </a>
                 </div>
@@ -32,9 +32,10 @@
                     <div class="display-4 fs-5 mb-2 font-weight-normal text-primary" id="UsageCount">
                         {{ $Usage }}
                     </div>
-                    <a class="font-weight-bold text-nowrap fs-10" href="../app/e-commerce/customers.html">
-                        See all
-                        <span class="fas fa-angle-right mr-1"></span>
+                    <a class="font-weight-bold text-nowrap fs-10" href="#" id="BtnModalPemakaian">
+                        Lihat Detail
+                        <span class="fas
+                        fa-angle-right mr-1"></span>
                     </a>
                 </div>
             </div>
@@ -51,8 +52,8 @@
                     <div class="display-4 fs-5 mb-2 font-weight-normal text-warning" id="TagihanCount">
                         {{ $Tagihan }}
                     </div>
-                    <a class="font-weight-bold text-nowrap fs-10" href="../app/e-commerce/customers.html">
-                        See all
+                    <a class="font-weight-bold text-nowrap fs-10" href="#" id="BtnModalTagihan">
+                        Lihat Detail
                         <span class="fas fa-angle-right mr-1"></span>
                     </a>
                 </div>
@@ -136,6 +137,174 @@
 @endsection
 
 @section('modal')
+    <div class="modal fade" id="ModalInstalasi" tabindex="-1" role="dialog" aria-labelledby="ModalInstalasiLabel"
+        aria-modal="false">
+        <div class="modal-dialog modal-dialog-scrollable modal-fullscreen" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalInstalasiLabel">Daftar Instalasi</h5>
+                </div>
+                <div class="modal-body">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <ul id="tabs" class="nav nav-pills justify-content-center">
+                                    <li class="nav-item">
+                                        <a href="" data-target="#Permohonan" data-toggle="tab"
+                                            class="nav-link small text-uppercase active">
+                                            Permohonan
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="" data-target="#Pasang" data-toggle="tab"
+                                            class="nav-link small text-uppercase">
+                                            Pasang
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="" data-target="#Aktif" data-toggle="tab"
+                                            class="nav-link small text-uppercase">
+                                            Aktif
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div id="tabsContent" class="tab-content mt-3">
+                            <div id="Permohonan" class="tab-pane active show fade">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table class="table table-flush">
+                                            <thead class="thead-light">
+                                                <tr>
+                                                    <th>Kode Instalasi</th>
+                                                    <th>Customer</th>
+                                                    <th>Alamat</th>
+                                                    <th>Paket</th>
+                                                    <th>Tanggal Order</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="TablePermohonan"></tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="Pasang" class="tab-pane fade">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table class="table table-flush">
+                                            <thead class="thead-light">
+                                                <tr>
+                                                    <th>Kode Instalasi</th>
+                                                    <th>Customer</th>
+                                                    <th>Alamat</th>
+                                                    <th>Paket</th>
+                                                    <th>Tanggal Order</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="TablePasang"></tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                            <div id="Aktif" class="tab-pane fade">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table class="table table-flush">
+                                            <thead class="thead-light">
+                                                <tr>
+                                                    <th>Kode Instalasi</th>
+                                                    <th>Customer</th>
+                                                    <th>Alamat</th>
+                                                    <th>Paket</th>
+                                                    <th>Tanggal Aktif</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="TableAktif"></tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-primary btn-modal-close">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="ModalPemakaian" tabindex="-1" role="dialog" aria-labelledby="ModalPemakaianLabel"
+        aria-modal="false">
+        <div class="modal-dialog modal-dialog-scrollable modal-fullscreen" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalPemakaianLabel">Daftar Pemakaian</h5>
+                    <button type="button" class="close btn-modal-close" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th>Kode Instalasi</th>
+                                        <th>Customer</th>
+                                        <th>Paket</th>
+                                        <th>Pemakaian</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="TablePemakaian"></tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-primary btn-modal-close">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="ModalTagihan" tabindex="-1" role="dialog" aria-labelledby="ModalTagihanLabel"
+        aria-modal="false">
+        <div class="modal-dialog modal-dialog-scrollable modal-fullscreen" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="ModalTagihanLabel">Daftar Tagihan</h5>
+                    <button type="button" class="close btn-modal-close" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="card">
+                        <div class="card-body">
+                            <table class="table table-flush">
+                                <thead class="thead-light">
+                                    <tr>
+                                        <th>Kode Instalasi</th>
+                                        <th>Customer</th>
+                                        <th>Tgl Tagihan</th>
+                                        <th>Jumlah</th>
+                                        <th>Tagihan</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="TableTagihan"></tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-primary btn-modal-close">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('script')
@@ -169,39 +338,160 @@
     </script>
 
     <script>
-        function dataInstallations() {
-            $.ajax({
+        async function dataInstallations() {
+            var result = await $.ajax({
                 'url': '/dashboard/installations',
                 'type': 'GET',
                 'dataType': 'json',
                 'success': function(result) {
-                    console.log(result);
+                    return result
                 }
             })
+
+            return result;
         }
 
-        function dataUsages() {
-            $.ajax({
+        async function dataUsages() {
+            var result = await $.ajax({
                 'url': '/dashboard/usages',
                 'type': 'GET',
                 'dataType': 'json',
                 'success': function(result) {
-                    console.log(result);
+                    return result
                 }
             })
+
+            return result;
         }
 
-        function dataTagihan() {
-            $.ajax({
+        async function dataTagihan() {
+            var result = await $.ajax({
                 'url': '/dashboard/tagihan',
                 'type': 'GET',
                 'dataType': 'json',
                 'success': function(result) {
-                    console.log(result);
+                    return result
                 }
             })
+
+            return result;
         }
 
-        dataTagihan()
+        $(document).on('click', '#BtnModalInstalasi', async function(e) {
+            e.preventDefault();
+            var result = await dataInstallations();
+
+            var Permohonan = result.Permohonan;
+            var Pasang = result.Pasang;
+            var Aktif = result.Aktif;
+
+            $('#TablePermohonan').html('');
+            Permohonan.forEach((item, index) => {
+                $('#TablePermohonan').append(`
+                    <tr>
+                        <td>${item.kode_instalasi}</td>
+                        <td>${item.customer.nama}</td>
+                        <td>${item.customer.alamat}</td>
+                        <td>${item.package.kelas}</td>
+                        <td>${item.order}</td>
+                    </tr>
+                `)
+            })
+
+            $('#TablePasang').html('');
+            Pasang.forEach((item, index) => {
+                $('#TablePasang').append(`
+                    <tr>
+                        <td>${item.kode_instalasi}</td>
+                        <td>${item.customer.nama}</td>
+                        <td>${item.customer.alamat}</td>
+                        <td>${item.package.kelas}</td>
+                        <td>${item.order}</td>
+                    </tr>
+                `)
+            })
+
+            $('#TableAktif').html('');
+            Aktif.forEach((item, index) => {
+                $('#TableAktif').append(`
+                    <tr>
+                        <td>${item.kode_instalasi}</td>
+                        <td>${item.customer.nama}</td>
+                        <td>${item.customer.alamat}</td>
+                        <td>${item.package.kelas}</td>
+                        <td>${item.aktif}</td>
+                    </tr>
+                `)
+            })
+
+            $('#ModalInstalasi').modal('toggle');
+        });
+
+        $(document).on('click', '#BtnModalPemakaian', async function(e) {
+            e.preventDefault();
+            var result = await dataUsages();
+            var Pemakaian = result.Usages;
+
+            var data = 0;
+            var empty = 0;
+            $('#TablePemakaian').html('');
+            Pemakaian.forEach((item, index) => {
+                if (item.one_usage == null) {
+                    empty += 1;
+                } else {
+                    $('#TablePemakaian').append(`
+                        <tr>
+                            <td>${item.kode_instalasi}</td>
+                            <td>${item.customer.nama}</td>
+                            <td>${item.package.kelas}</td>
+                            <td>${item.one_usage.akhir}</td>
+                        </tr>
+                    `)
+                }
+
+                data += 1
+            })
+
+            if (data - empty == 0) {
+                $('#TablePemakaian').append(`
+                        <tr>
+                            <td align="center" colspan="4">Tidak ada data pemakaian</td>
+                        </tr>
+                    `)
+            }
+
+            $('#ModalPemakaian').modal('toggle');
+        });
+
+        $(document).on('click', '#BtnModalTagihan', async function(e) {
+            e.preventDefault();
+            var result = await dataTagihan();
+            var Tagihan = result.Tagihan;
+            var setting = result.setting;
+            var block = result.block
+
+
+            $('#TableTagihan').html('');
+            Tagihan.forEach((item, index) => {
+                var paket = JSON.parse(item.installation.package.harga)
+                $('#TableTagihan').append(`
+                <tr>
+                    <td>${item.installation.kode_instalasi}</td>
+                    <td>${item.installation.customer.nama}</td>
+                    <td>${item.tgl_akhir}</td>
+                    <td>${item.jumlah}</td>
+                    <td>${paket[block[item.jumlah]]}</td>
+                </tr>
+            `)
+            })
+
+            $('#ModalTagihan').modal('toggle');
+        });
+
+        $(document).on('click', '.btn-modal-close', function(e) {
+            e.preventDefault();
+
+            $('.modal').modal('hide');
+        });
     </script>
 @endsection
