24e8d02c9d5c18eb2b722ceb763020b6dbeb2ff1
arafi118 <arp46301@gmail.com>
2025-01-26 19:17:11 +0700

    SidebarButton

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -97,6 +97,7 @@
         crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/echarts@5.6.0/dist/echarts.min.js"></script>
+    <script src="/assets/js/demo/ruang-admin.js"></script>
 
     {{-- Logout --}}
     <script>
