94ff21ddac4ae063808248ba98dcca1c04b22d3c
farilraehan <farilraihan50@gmail.com>
2025-04-16 12:48:57 +0700

    cetak pemakaian

--- resources/views/perguliran/partials/cetak.blade.php
+++ resources/views/perguliran/partials/cetak.blade.php
@@ -11,28 +11,26 @@
     <title>Cetak Pemakaian</title>
     <style>
         body {
-            font-size: 12px;
+            font-size: 10px;
             font-family: Arial, sans-serif;
             width: 10cm;
             height: 15cm;
-            margin: 1px auto;
-            padding: 1px;
+            margin: 0 auto;
+            padding: 0;
             text-align: center;
             position: relative;
         }
 
         .container {
-            width: 130%;
-            padding: 10px;
-            border: 2px solid rgb(0, 0, 0);
-            position: relative;
-            left: 50%;
-            transform: translateX(-50%);
+            width: 100%;
+            padding: 5px;
+            border: 1px solid rgb(0, 0, 0);
+            box-sizing: border-box;
         }
 
         .header img {
-            width: 40px;
-            height: 40px;
+            width: 30px;
+            height: 30px;
         }
 
         .header,
@@ -54,7 +52,8 @@
         .content th,
         .content td {
             border: 1px solid #000;
-            padding: 2px;
+            padding: 1px;
+            font-size: 9px;
             text-align: center;
         }
 
@@ -77,10 +76,9 @@
         th,
         td {
             border: 1px solid #000;
-            padding: 5px;
+            padding: 2px;
         }
 
-        /* Header */
         thead tr th:first-child {
             border-top-left-radius: 10px;
             text-align: center;
@@ -90,7 +88,6 @@
             text-align: left;
         }
 
-        /* Isi kolom */
         tbody tr td:first-child {
             text-align: center;
         }
@@ -99,190 +96,128 @@
             text-align: left;
         }
 
-        /* Pojok kiri bawah */
         tbody tr:last-child td:first-child {
             border-bottom-left-radius: 10px;
         }
 
         .content tbody td:nth-child(2) {
-            padding-top: 8px;
-            padding-bottom: 8px;
+            padding-top: 5px;
+            padding-bottom: 5px;
+        }
+
+        img {
+            max-height: 40px !important;
+        }
+
+        .content tbody tr {
+            height: 6.5mm;
         }
     </style>
-
 </head>
 
 <body onload="window.print()">
     <div class="container">
         <div class="header">
-            <div style="font-size: 14px"></div>
+            <div style="font-size: 12px"></div>
             <div></div>
         </div><br>
 
         <div style="position: relative;">
-            <!-- Tabel utama: QR + Data Pelanggan -->
             <div style="position: relative; text-align: center;">
-                <table style="width: 100%;">
+                <table style="width: 100%; border-collapse: collapse;">
                     <tr>
-                        <td width="20%" align="right" style="border: none;">
-                            {{-- <img src="../storage/app/public/logo/{{ $gambar }}" width="50" height="50"> --}}
-                            <img src="../../assets/img/cetak1.png"
-                                style="max-height: 50px; margin-right: 15px; margin-left: 10px;"
-                                class="img-fluid  d-none d-lg-block">
+                        <td colspan="3" style="text-align: left; border: none;">
+                            <div style="font-size: 12px;">KARTU METER PENGGUNAAN AIR</div>
+                            <div style="font-size: 14px;"><b>UNIT AIR {{ strtoupper($bisnis->nama) }}</b></div>
+                            <div style="font-size: 11px;">(BUMDes) BANGUN KENCANA KALURAHAN
+                                {{ strtoupper($bisnis->desa) }}</div>
                         </td>
-
-                        <td width="60%" align="center" style="height: 50px; border: none;">
-                            <!-- Placeholder kosong agar posisi tetap -->
-                        </td>
-                        <td width="20%" align="left" style="border: none;">
-                            {{-- <img src="../storage/app/public/logo/{{ $gambar }}" width="50" height="50"> --}}
-                            <img src="../../assets/img/cetak2.png" style="max-height: 50px; margin-left: 10px;"
-                                class="img-fluid d-none d-lg-block">
+                        <td style="text-align: right; border: none; white-space: nowrap;">
+                            <img src="../../assets/img/cetak2.png" style="vertical-align: middle; margin-right: 2px;"
+                                alt="Logo 2">
+                            <img src="{{ '/storage/logo/' . $gambar }}" style="vertical-align: middle;" alt="Logo 1">
                         </td>
                     </tr>
                 </table>
-
-
-                <!-- Judul Tengah -->
-                <div
-                    style="position: absolute; top: 0; left: 50%; transform: translateX(-50%); text-align: center; max-width: 100%;">
-                    <div style="font-size: 13px;">KARTU METER PENGGUNAAN AIR</div>
-                    <div style="font-size: 16px;"><b>UNIT AIR "TIRTO MULO"</b></div>
-                    <div style="font-size: 13px; word-wrap: break-word;">(BUMDes) BANGUN KENCANA KALURAHAN MULO</div>
-                </div>
             </div>
+
             <table style="width: 100%; border-collapse: collapse; text-align: left;">
                 <tr>
-                    <td colspan="3" style="border: none;">
-                        <div style="border-top: 3px solid rgb(88, 86, 86); margin-bottom: 5px; width: 100%;"></div>
+                    <td colspan="3" style="border: none;">&nbsp;
                     </td>
                 </tr>
                 <tr>
-                    <td rowspan="3" style="width: 70px; vertical-align: top; border: none;">
-                        <div style="width: 60px;">
+                    <td colspan="3" style="border: none;">
+                        <div style="border-top: 2px solid rgb(88, 86, 86); margin-bottom: 3px; width: 100%;"></div>
+                    </td>
+                </tr>
+                <tr>
+                    <td rowspan="3" style="width: 60px; vertical-align: top; border: none;">
+                        <div style="width: 50px;">
                             {!! $qr !!}
                         </div>
                     </td>
-                    <th style="width: 30%; text-align: left; border: none;">NAMA PELANGGAN</th>
-                    <th style="border: none;">: ..................................................</th>
+                    <td style="width: 30%; text-align: left; border: none;">NAMA PELANGGAN</td>
+                    <td style="border: none;">: {{ $installation->customer->nama }}</td>
                 </tr>
                 <tr>
-                    <th style="text-align: left; border: none;">NO. INDUK</th>
-                    <th style="border: none;">: ..................................................</th>
+                    <td style="text-align: left; border: none;">NO. INDUK</td>
+                    <td style="border: none;">: {{ $installation->kode_instalasi }}</td>
                 </tr>
                 <tr>
-                    <th style="text-align: left; border: none;">ALAMAT</th>
-                    <th style="border: none;">: ..................................................</th>
+                    <td style="text-align: left; border: none;">ALAMAT</td>
+                    <td style="border: none;">: {{ $installation->village->nama }}</td>
                 </tr>
             </table>
         </div>
+        <tr>
+            <td colspan="3" style="border: none;">&nbsp;
+            </td>
+        </tr>
         <div class="content">
             <table>
                 <thead>
                     <tr>
-                        <thead>
-                            <tr>
-                                <th rowspan="2" style="text-align: center;">NO</th>
-                                <th rowspan="2" style="text-align: center;">BULAN</th>
-                                <th rowspan="2" style="text-align: center;">ANGKA METER</th>
-                                <th rowspan="2" style="text-align: center;">TTD CATER</th>
-                                <th rowspan="2" style="text-align: center;">KETERANGAN</th>
-                            </tr>
-                            <tr>
-                                <!-- baris kosong karena semua kolom di atas sudah digabung 2 baris -->
-                            </tr>
-                        </thead>
-
-
+                        <th rowspan="2"
+                            style="text-align: center; vertical-align: middle; padding-top: 14px; padding-bottom: 10px;">
+                            NO</th>
+                        <th rowspan="2"
+                            style="text-align: center; vertical-align: middle; padding-top: 10px; padding-bottom: 10px;">
+                            BULAN</th>
+                        <th rowspan="2" style="text-align: center; vertical-align: middle;">ANGKA METER</th>
+                        <th rowspan="2" style="text-align: center; vertical-align: middle;">TTD CATER</th>
+                        <th rowspan="2" style="text-align: center; vertical-align: middle;">KETERANGAN</th>
 
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>JANUARI</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>FEBRUARI</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>MARET</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>APRIL</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>5</td>
-                        <td>MEI</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>6</td>
-                        <td>JUNI</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>7</td>
-                        <td>JULI</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>8</td>
-                        <td>AGUSTUS</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>9</td>
-                        <td>SEPTEMBER</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>10</td>
-                        <td>OKTOBER</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>11</td>
-                        <td>NOVEMBER</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td>12</td>
-                        <td>DESEMBER</td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
+                    @foreach (['JANUARI', 'FEBRUARI', 'MARET', 'APRIL', 'MEI', 'JUNI', 'JULI', 'AGUSTUS', 'SEPTEMBER', 'OKTOBER', 'NOVEMBER', 'DESEMBER'] as $i => $bulan)
+                        <tr>
+                            <td>{{ $i + 1 }}</td>
+                            <td>{{ $bulan }}</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    @endforeach
                 </tbody>
+            </table>
+            <tr>
+                <td>&nbsp;</td>
+            </tr>
+            <table style="width: 100%; border-collapse: collapse; text-align: center; margin: 5px 0;">
+                <tr>
+                    <td colspan="5" style="border: none; font-size: 11px; padding-top: 3px; padding-bottom: 3px;">
+                        <br>
+                        <b>INFORMASI DAN KELUHAN HUBUNGI</b>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="5" style="border: none; font-size: 11px; padding-bottom: 8px;">
+                        <b>Heru Endaryanto: 087838758555 / Widiyarto: 087878715088</b>
+                    </td>
+                </tr>
+
             </table>
         </div>
     </div>
