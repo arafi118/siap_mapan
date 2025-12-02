3822fb0017ef54deefbd3335fc9daa3e66f7e5e4
arafi118 <arp46301@gmail.com>
2025-12-02 14:19:04 +0700

    🐛 fix(penggunaan): refine penalty calculation logic for usage receipts
    - introduce `$tgl_akhhir_lalu` to capture the month of the usage end date
    - implement check for unpaid status of the previous usage record
    - modify penalty transaction source from `$use->installation` to `$installations`
    - update penalty transaction condition to filter by month using `$tgl_akhhir_lalu`

--- resources/views/penggunaan/partials/cetak.blade.php
+++ resources/views/penggunaan/partials/cetak.blade.php
@@ -49,9 +49,17 @@
     <div class="container">
         @foreach ($usage as $use)
             @php
+                $tgl_akhhir_lalu = date('Y-m', strtotime('-0 month', strtotime($usage->tgl_akhir)));
+                $isUnpaid = false;
+                if (isset($usages[$index - 1])) {
+                    $isUnpaid = $usages[$index - 1]->status == 'UNPAID' ? true : false;
+                }
                 $dendaPemakaianLalu = 0;
-                foreach ($use->installation->transaction as $trx_denda) {
-                    if ($trx_denda->tgl_transaksi < $use->tgl_akhir) {
+                foreach ($installations->transaction as $trx_denda) {
+                    if (
+                        $trx_denda->tgl_transaksi < $usage->tgl_akhir &&
+                        date('Y-m', strtotime($trx_denda->tgl_transaksi)) == $tgl_akhhir_lalu
+                    ) {
                         $dendaPemakaianLalu = $trx_denda->total;
                     }
                 }
